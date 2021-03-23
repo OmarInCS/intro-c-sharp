d2d513268f1db77878ac0cec5008e18af806ec2a
OmarInCS <omarincs@gmail.com>
2021-03-23 20:05:38 +0300

    Variables & Datatypes

---
+++ Group20210321/week1/day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group20210321
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    var x = 0;
+        //    Console.WriteLine("Welcome to C#");
+        //    Console.WriteLine("C# is Fun");
+        //    //Console.WriteLine(5 / x);     Runtime Error
+        //    //Console.WriteLine(5 / 2);     Logic Error
+        //    //Console.WriteLine("5 + 5 => " + 5 + 5);   Logic Error
+        //}
+    }
+}
---
+++ Group20210321/week1/day2/ComputeSalary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group20210321.week1.day2
+{
+    class ComputeSalary
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your salary: ");
+            double salary = double.Parse(Console.ReadLine());
+            double annualSalary = salary * 12;
+            Console.WriteLine("Annual Salary: " + annualSalary);
+
+        }
+    }
+}
